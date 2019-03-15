26a9e177f0a778bf8c64286b25763fb817681e2f
张宁安 <zhangningan@yodoo.net.cn>
2019-03-15 15:59:01 +0800

    client配置修改；新增git配置文件

--- client/src/main/java/com/example/client/EurekaClientApplication.java
+++ client/src/main/java/com/example/client/EurekaClientApplication.java
@@ -3,27 +3,29 @@ package com.example.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableEurekaClient
+//@EnableEurekaClient
 @RestController
-
+@RefreshScope
 public class EurekaClientApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
     }
 
-    @Value("${server.port}")
-    String port;
+    //    @Value("${server.port}")
+//    @Value("${spring.application.name}")
+    @Value("${hello}")
+    String hello;
 
-    @RequestMapping("/")
-    public String home() {
-        return "hello world from port " + port;
-        //26
+    @RequestMapping(value = "/hello")
+    public String hello() {
+        return hello;
     }
 
 }
